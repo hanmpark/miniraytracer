441b683c676408950977d09ef92533d343bb27d1
Hanmin <hanmin.01.park@gmail.com>
2026-01-19 20:54:40 +0100

    fix(cone): correct normal calculations and intersection logic

--- src/objects/mrt_cone.c
+++ src/objects/mrt_cone.c
@@ -8,13 +8,12 @@ static void	hit_surface_normal(t_hit *hit, t_fvec3 ray, t_obj *o, t_type type)
 	if (type == SIDE)
 	{
 		compute_spheric_uv(hit, ray, CONE);
-		obj_normal = new_fvec3(ray.x, ray.y, \
-			sqrt((ray.x * ray.x) + (ray.y * ray.y)));
+		obj_normal = new_fvec3(ray.x, ray.y, -ray.z);
 	}
 	else
 	{
 		compute_planar_uv(hit, ray, CONE);
-		obj_normal = new_fvec3(0.0, 0.0, -1.0);
+		obj_normal = new_fvec3(0.0, 0.0, (ray.z < 0.0) ? -1.0 : 1.0);
 	}
 	hit->normal = norm_fvec3(mult_mat3x3_fvec3(obj_normal, o->lintfm));
 }
@@ -30,7 +29,7 @@ static double	side_isect(t_fvec3 pos, t_fvec3 dir, t_fvec3 *ray)
 	if (root == INFINITY)
 		return (INFINITY);
 	*ray = add_fvec3(pos, mult_double_fvec3(dir, root));
-	if (ray->z <= -1.0 || ray->z >= 0.0)
+	if (ray->z <= -1.0 || ray->z >= 1.0)
 		return (INFINITY);
 	return (root);
 }
@@ -38,11 +37,19 @@ static double	side_isect(t_fvec3 pos, t_fvec3 dir, t_fvec3 *ray)
 static double	cap_isect(t_fvec3 pos, t_fvec3 dir, t_fvec3 *ray)
 {
 	double	root;
+	double	root1;
+	double	root2;
 
 	if (close_enough(dir.z, 0.0))
 		return (INFINITY);
-	root = (pos.z + 1.0) / -dir.z;
-	if (root <= 0.0)
+	root1 = (pos.z + 1.0) / -dir.z;
+	root2 = (pos.z - 1.0) / -dir.z;
+	root = INFINITY;
+	if (root1 > 0.0)
+		root = root1;
+	if (root2 > 0.0 && root2 < root)
+		root = root2;
+	if (root == INFINITY)
 		return (INFINITY);
 	*ray = add_fvec3(pos, mult_double_fvec3(dir, root));
 	if (sqrt((ray->x * ray->x) + (ray->y * ray->y)) >= 1.0)
